c5d5ee3a6c228850f842bc6fc1eba0b66a42d18b
Roberto Pedroso <roberto@rpedroso.com>
2015-02-17 23:46:04 -0500

    solved problem 13

---
+++ problem13.py
@@ -0,0 +1,14 @@
+import os
+
+if __name__ == "__main__":
+    # safely load script without caring about CWD
+    script_dir = os.path.dirname(__file__)
+    data_path = "data/problem13.txt"
+    fpath = os.path.join(script_dir, data_path)
+
+    # this uninspired question gets an uninspired solution
+    # python tacitly converts ints to longs to save us the trouble
+    with open(fpath, 'r') as f:
+        bigsum = sum(int(line) for line in f)
+        firstn = str(bigsum)[0:10]
+        print firstn
