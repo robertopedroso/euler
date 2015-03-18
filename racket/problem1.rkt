4306b0d3f029284862544bd5f9cbdc277b32a6db
Roberto Pedroso <roberto@rpedroso.com>
2015-03-18 15:52:37 -0400

    adding util lib and pointing first problem to those utils

--- racket/problem1.rkt
+++ racket/problem1.rkt
@@ -1,6 +1,5 @@
 #lang racket
-(define (sum lst)
-  (cond
-    [(empty? lst) 0]
-    [else (+ (first lst) (sum (rest lst)))]))
+
+(require "util.rkt")
+
 (sum (filter (lambda (x) (or (= 0 (modulo x 3)) (= 0 (modulo x 5)))) (range 1 1000)))
---
+++ racket/util.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(provide sum)
+(provide define-memoized)
+
+; sum a list of numbers
+(define (sum lst)
+  (cond
+    [(empty? lst) 0]
+    [else (+ (first lst) (sum (rest lst)))]))
+
+; memoized version of define
+; from: http://blog.jverkamp.com/2012/10/20/memoization-in-racket/
+(define-syntax define-memoized
+  (syntax-rules ()
+    [(_ (f args ...) bodies ...)
+     (define f
+       (let ([results (make-hash)])
+         (lambda (args ...)
+           ((lambda vals
+              (when (not (hash-has-key? results vals))
+                (hash-set! results vals (begin bodies ...)))
+              (hash-ref results vals))
+            args ...))))]))
