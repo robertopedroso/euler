65a989143ef36f4e21b7c83b2fdafd2be57dae33
Roberto Pedroso <roberto@rpedroso.com>
2015-03-18 15:52:43 -0400

    solved problem 2

---
+++ racket/problem2.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(require "util.rkt")
+
+(define-memoized (fib n)
+  (cond
+    [(= n 0) 0]
+    [(= n 1) 1]
+    [else (+ (fib (- n 1)) (fib (- n 2)))]))
+
+(sum (filter even? (map fib (range 1 34))))
